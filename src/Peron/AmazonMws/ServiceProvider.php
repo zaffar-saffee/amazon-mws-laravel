561740d74c5e4788cbb997aa2d3cdf6ed93d4977
Zaffar Saffee <zaffar4fnds@gmail.com>
2016-04-29 01:05:55 +0500

    persistent settings

--- src/Peron/AmazonMws/ServiceProvider.php
+++ src/Peron/AmazonMws/ServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Peron\AmazonMws;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use anlutro\LaravelSettings\Facade as Setting;
 
 class ServiceProvider extends BaseServiceProvider
 {
@@ -19,17 +20,21 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
-        $configPath = __DIR__ . '/../../config/amazon-mws.php';
-        $this->mergeConfigFrom($configPath, 'amazon-mws');
+        /*$configPath = __DIR__ . '/../../config/amazon-mws.php';
+        $this->mergeConfigFrom($configPath, 'amazon-mws');*/
+        $this->readyConfig ();
 
         $this->app->alias('AmazonOrderList', 'Peron\AmazonMws\AmazonOrderList');
+        $this->app->alias('AmazonOrderItemList', 'Peron\AmazonMws\AmazonOrderItemList');
+        $this->app->alias('AmazonReportRequest', 'Peron\AmazonMws\AmazonReportRequest');
+        $this->app->alias('AmazonReportList', 'Peron\AmazonMws\AmazonReportList');
         $this->app->alias('AmazonOrderItemList', 'Peron\AmazonMws\AmazonOrderItemList');
     }
 
     public function boot()
     {
-        $configPath = __DIR__ . '/../../config/amazon-mws.php';
-        $this->publishes([$configPath => config_path('amazon-mws.php')], 'config');
+        /*$configPath = __DIR__ . '/../../config/amazon-mws.php';
+        $this->publishes([$configPath => config_path('amazon-mws.php')], 'config');*/
     }
 
     /**
@@ -40,6 +45,32 @@ class ServiceProvider extends BaseServiceProvider
     public function provides()
     {
         return array();
+    }
+
+    /**
+     * take array and
+     */
+    private function readyConfig ()
+    {
+        $configStore = [
+            'store' => [
+                Setting::get('storeName') => [
+                    'merchantId' => Setting::get('merchantId'),
+                    'marketplaceId' => Setting::get('marketplaceId'),
+                    'keyId' => Setting::get('keyId'),
+                    'secretKey' => Setting::get('secretKey'),
+                    'amazonServiceUrl'=> Setting::get('amazonServiceUrl'),
+                ]
+            ],
+
+            // Default service URL
+            'AMAZON_SERVICE_URL' => 'https://mws.amazonservices.com/',
+
+            'muteLog' => false
+        ];
+        $key= 'amazon-mws';
+        $this->app['config']->set($key,  $configStore);
+        
     }
 
 }
